6e4d5d0f54bd694edb3b1e6389f6c98c40735ab4
ManpreetKaur1213 <63481510+ManpreetKaur1213@users.noreply.github.com>
2020-09-18 13:24:03 +1200

    There is no any modification of code

--- app/Role.php
+++ app/Role.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    public function role($name)
+    {
+        return $this->name=$name;
+    }
     /**
      * The attributes that are mass assignable.
      *
