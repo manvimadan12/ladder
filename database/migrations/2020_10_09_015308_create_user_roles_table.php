cfab057a9b3c055405aeab87d4392b03cf56f596
ManpreetKaur1213 <63481510+ManpreetKaur1213@users.noreply.github.com>
2020-10-14 15:02:24 +1300

    Renamed Table name
    users_roles Table name has changed to user_roles

--- database/migrations/2020_10_09_015308_create_user_roles_table.php
+++ database/migrations/2020_10_09_015308_create_user_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersRolesTable extends Migration
+class CreateUserRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_roles', function (Blueprint $table) {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->id();
             $table->integer('role_id');
             $table->integer('user_id');
-            $table->primary(['role_id', 'user_id']);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreateUsersRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_roles');
+        Schema::dropIfExists('user_roles');
     }
 }
