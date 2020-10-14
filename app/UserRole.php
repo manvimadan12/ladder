04dcc487f615d81d609e91c6a4e0943aadf83176
ManpreetKaur1213 <63481510+ManpreetKaur1213@users.noreply.github.com>
2020-10-14 15:08:04 +1300

    Created a class UserRole

--- app/UserRole.php
+++ app/UserRole.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserRole extends Model
 {
-    //
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
+
+
+
+
+
+
